3ddbbe94cf08b50c8182eba6ceb9f15a353aad50
phenricks <pedrohricksilva@gmail.com>
2021-07-11 13:43:56 -0300

    first commit: Organizando ações

---
+++ hello_friend.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##### ========================================================================
+##### NOME:              hello_friend.sh                                 #####
+##### VERSÃO:            0.1                                             #####
+##### DESCRIÇÃO:                                                         #####
+##### DATA DA CRIAÇÃO:   09/07/2021                                      #####
+##### ESCRITO POR:       Pedro Henrique                                  #####
+##### E-MAIL:            pedrohricksilva@gmail.com                       #####
+##### DISTRO:            20.04.1-Ubuntu                                  #####
+##### PROJETO:           https://gith.b.com/                             #####
+##### ========================================================================
+
+## Apresentação Terminal ##
+        #TODO
+# ----------------------------- VARIÁVEIS ----------------------------- #
+    #TODO
+    DIR_DOWNLOADS="$HOME/Downloads/Programas"
+# -------------------------------------------------------------------- #
+echo $DIR_DOWNLOADS
