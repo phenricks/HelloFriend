dcb4db4e55c0b288aa002ae3ee6b1c8915a46873
phenricks <pedrohricksilva@gmail.com>
2021-07-13 17:08:44 -0300

    Adicionado flatpak/ + programas

--- hello_friend.sh
+++ hello_friend.sh
@@ -14,27 +14,37 @@
 ##### ======================================================================== #####
 
 # ------------------------ APRESENTAÇÃO TERMINAL ---------------------- #
-    #TODO
-    
+    #TODO    
 # ----------------------------- VARIÁVEIS ----------------------------- #
-
+    REM_ADD_FLATPAK="https://flathub.org/repo/flathub.flatpakrepo"
     REMMINA_PPA="ppa:remmina-ppa-team/remmina-next"
     CHROME_URL="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
     
     DIR_DOWNLOADS="$HOME/Downloads/Programas"
     
-
     APPS_TO_INSTALL=(
-        virtualbox-qt
+        virtualbox
+        build-essential 
+        module-assistant
+        virtualbox-dkms
+        virtualbox-ext-pack
         ubuntu-restricted-extras
+        gnome-tweak-tool
+        git-all
+        net-tools
         remmina 
         remmina-plugin-rdp 
-        remmina-plugin-secret
-	openjdk-11-jre-headless
+        remmina-plugin-secret 
+        remmina-plugin-exec 
+        remmina-plugin-www  
+        remmina-plugin-kwallet
+        openssh-server
+        openjdk-11-jre-headless
+        flatpak
+        gnome-software-plugin-flatpak
     )
 
 # ---------------------------------------------------------------------- #
-
 
 # ------------------------- REQUISITOS BÁSICOS ------------------------- #
 
@@ -63,6 +73,7 @@ sudo dpkg -i $DIR_DOWNLOADS/*.deb
 
 for programa in ${APPS_TO_INSTALL[@]}; do
 	if ! dpkg -l | grep -q $programa; then
+		echo -e "\033[0;33m[INSTALANDO: $programa]\033[0m"
 		sudo apt install "$programa" -y
 	else
 		echo -e "\033[0;32m[INSTALADO]\033[0m - $programa"
@@ -71,19 +82,32 @@ done
 
 # ---------------------------------------------------------------------- #
 
-# ----------------------------- SNAPS ---------------------------------- #
+# ------------------------ SNAPS and FLATPAK---------------------------- #
 
+#SNAP
 sudo snap install spotify
 sudo snap install code --classic
 
+#FLATPAK
+flatpak remote-add --if-not-exists flathub "$REM_ADD_FLATPAK"
+flatpak install flathub com.discordapp.Discord -y
+
 # ----------------------------- FINALIZAÇÃO ----------------------------- #
 
-## Finalização, atualização e limpeza##
+## Finalização, atualização e limpeza ##
 sudo apt update && sudo apt dist-upgrade -y
-#flatpak update #TODO :Realizar instalação do flatpak
+flatpak update
 sudo apt autoclean
 sudo apt autoremove -y
 
-echo -e "\033[0;32m[FINALIZADO COM SUCESSO!]\033[0m"
+clear 
+
+echo -e "\033[0;32mPROCESSO FINALIZADO!\033[0m"
+echo -e "Confira a lista de Programas instalados:s"
+for programa in ${APPS_TO_INSTALL[@]}; do
+	if  dpkg -l | grep -q $programa; then
+		echo -e "\033[1;37m via:apt\033[0m - \033[0;32m[*]\033[0m - $programa"
+	fi
+done
 
 # ---------------------------------------------------------------------- #
